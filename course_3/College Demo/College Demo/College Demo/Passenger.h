8bf830fafb2ac3f6650bc96902c5b49f7577f3ce
lixiangwuxian <lixiangwuxian@outlook.com>
2022-07-27 19:55:56 +0800

    update course_3

--- course_3/College Demo/College Demo/College Demo/Passenger.h
+++ course_3/College Demo/College Demo/College Demo/Passenger.h
@@ -10,19 +10,36 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface Orders : NSObject
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic,strong) NSDate *date;
+@property (nonatomic,strong) NSString *origin;
+@property (nonatomic,strong) NSString *destination;
+@property (nonatomic,strong) NSInteger *status;
+// Function 方法
+// 去订票
+- (void)go_booking  
+    who_booking:(NSString *)person_name
+    origin:(NSString *)origin
+    destination:(NSString *)destination 
+    date:(NSDate *)date;
 
+// 去检票
+- (void)go_check_ticket 
 @end
 
 @interface Passenger : Person
 // @property 属性
 // 是否年满 18 岁
+@property (nonatomic,assign,readonly) NSInteger* age;
 // 历史订单 （数组）
+@property (nonatomic,strong) NSArray<Orders *> *history_orders;
 // 未出行订单 （数组）
-
-// Function 方法
-// 去订票
-
-// 去检票
+@property (nonatomic,strong) NSArray<Orders *> *untraveled_orders;
+- (void)creat_order 
+    order:(Orders *)order;
+- (void)use_order;
+- (void)cancel_order
+    order:(Orders *)order;
 @end
 
 NS_ASSUME_NONNULL_END
--- course_3/College Demo/College Demo/College Demo/Passenger.m
+++ course_3/College Demo/College Demo/College Demo/Passenger.m
@@ -9,9 +9,46 @@
 
 
 @implementation Orders
+- (void)go_booking  
+who_booking:(NSString *)person_name
+origin:(NSString *)origin
+destination:(NSString *)destination 
+date:(NSDate *)date
+{
+    NSLog(@"%@ is booking %@ to %@ on %@", person_name,origin, destination, date);
+    self.person_name = person_name;
+    self.origin = origin;
+    self.destination = destination;
+    self.date = date;
+    self.status = false;//unchecked
+}
+
+- (void)go_check_ticket 
+{
+    NSLog(@"%@ is checking ticket at %@", self.person_name, self.date);
+    self.status = true;//checked
+}
 
 @end
 
 @implementation Passenger
+- (void)creat_order 
+order:(Orders *)order
+{
+    [self.untraveled_orders addObject:order];
+}
 
+- (void)use_order
+{
+    Orders *order = [self.untraveled_orders firstObject];
+    [self.untraveled_orders removeObject:order];
+    [self.traveled_orders addObject:order];
+    order.go_check_ticket();
+}
+
+- (void)cancel_order
+order:(Orders *)order
+{
+    [self.untraveled_orders removeObject:order];
+}
 @end
